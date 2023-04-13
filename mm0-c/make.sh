e3f9c3ce04e7efa253b3484da7527fb99a77e60c
Mario Carneiro <di.gama@gmail.com>
2023-04-12 21:55:41 -0400

    account for varying gcda names in PGO
    fixes #124

--- mm0-c/make.sh
+++ mm0-c/make.sh
@@ -8,4 +8,4 @@ set -e
 gcc main.c -O2 -Wall -fprofile-generate -o mm0-c
 ./mm0-c ../examples/peano.mmb < ../examples/peano.mm0
 gcc main.c -O2 -Wall -fprofile-use -o mm0-c
-rm mm0-c-main.gcda
+rm -f mm0-c-main.gcda main.gcda
