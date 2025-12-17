034b61b116da646ab23937988df980e6f845deff
Mario Carneiro <di.gama@gmail.com>
2025-12-17 22:30:33 +0100

    add tests to build.yml

--- tests/mm0_mmu/run-mm0-rs.sh
+++ tests/mm0_mmu/run-mm0-rs.sh
@@ -5,7 +5,7 @@
 
 base=${1%.*}
 if [ -f $base.mmu ]; then
-  mm0-rs compile $base.mmu $base.mmb && ../../mm0-c/mm0-c $base.mmb < $base.mm0
+  mm0-rs compile $base.mmu $base.mmb && mm0-c $base.mmb < $base.mm0
 else
   mm0-rs compile $base.mm0
 fi
--- tests/mmb/run.sh
+++ tests/mmb/run.sh
@@ -4,7 +4,7 @@
 #              or fail/TEST
 
 if [ -f $1.mm0 ]; then
-  ../../mm0-c/mm0-c $1.mmb < $1.mm0
+  mm0-c $1.mmb < $1.mm0
 else
-  ../../mm0-c/mm0-c $1.mmb < /dev/null
+  mm0-c $1.mmb < /dev/null
 fi
